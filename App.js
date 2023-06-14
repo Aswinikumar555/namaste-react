1483581996caeb0f1fbbb5bc6c4757132426111a
aswini kumar <aswini.kumar@vedantu.com>
2023-06-14 19:12:51 +0530

    episode 3

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// core react
+// React.createElement => ReactElement Js Object => HTMLElement(render)
 const heading = React.createElement("h1", {}, "hellow world from react");
 console.log("heading ", heading);
 const parent = React.createElement(
@@ -11,6 +13,37 @@ const parent = React.createElement(
     React.createElement("h2", {}, "h2"),
   ])
 );
+
+// JSX (Transpile before it reaches to JS Engine) - PARCEL - Babel
+// JSX =>  React.createElement =>  ReactElement Js Object => HTMLElement(render)
+
+// React Element
+const jsxHeading = (
+  <h1 id="jsx-head" className="jsx-class" tabIndex="1">
+    jsx heading
+  </h1>
+);
+
+// React Component
+const TitleComponent = () => <div>Title</div>;
+
+// component Composition
+const HeadingComponent = () => {
+  return (
+    <div id="component-div">
+      <TitleComponent />
+      {TitleComponent()}
+      {<h2>second heading</h2>}
+      HeadingComponent
+    </div>
+  );
+};
+console.log("jsxHeading ", jsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+// root.render(jsxHeading);
+// root.render(HeadingComponent());
+
+root.render(<HeadingComponent />);
+root2.render(<HeadingComponent />);
